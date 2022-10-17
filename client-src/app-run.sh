6f57fd4e87746f67a7d26efb39274399b0913044
officialarijit <nandiarijit99@gmail.com>
2022-10-17 12:01:00 +0200

    updated 2022

--- client-src/app-run.sh
+++ client-src/app-run.sh
@@ -11,5 +11,6 @@ sudo docker run -d -v /home/ubuntu/data:/home/data \
 	                -v /home/ubuntu/fedserver-data/local_model:/home/local_model --env-file config.env --name "client$c" arijitnandi/fedclient $c 
 
 done
+
 echo "DOCKER STAT LOGGING...."
-while true; do sudo docker stats --all --no-stream --format "table {{.ID}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.PIDs}}" | cat >> ./`date -u +"localclient_stats.csv"`; sleep 10; done
+while true; do sudo docker stats --all --no-stream --format "table {{.ID}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.PIDs}}" | cat >> ./`date -u +"localclient_stats.csv"`; sleep 1; done
